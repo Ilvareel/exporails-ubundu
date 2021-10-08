fb46b51a63dbd9ac532cee14967a883037736fe8
lukas <lukas.moravcik.11@gmail.com>
2021-10-08 18:37:46 +0200

    new file:   .psql_history 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/users.scss 	new file:   app/controllers/users_controller.rb 	new file:   app/helpers/users_helper.rb 	modified:   app/javascript/packs/application.js 	new file:   app/models/user.rb 	new file:   app/views/users/index.html 	modified:   config/database.yml 	modified:   config/environments/development.rb 	new file:   config/initializers/rollbar.rb 	modified:   config/routes.rb 	modified:   config/webpack/environment.js 	new file:   db/migrate/20211008133612_create_users.rb 	new file:   db/schema.rb 	modified:   db/seeds.rb 	modified:   hacks.txt 	modified:   package.json 	new file:   test/controllers/users_controller_test.rb 	new file:   test/fixtures/users.yml 	new file:   test/models/user_test.rb 	modified:   yarn.lock

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("bootstrap/dist/js/bootstrap")
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
